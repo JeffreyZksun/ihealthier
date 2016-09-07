3c99cf1427aa2471da2fed5aa2bbdf8ccd6ca2ff
jeffrey.sun <jeffrey.sun@autodesk.com>
2016-09-07 15:51:15 +0800

    Update data

--- docs/components/data/controller.js
+++ docs/components/data/controller.js
@@ -8,18 +8,18 @@ app.controller('dataController', [
 '$location',
 function($rootScope, $scope, $routeParams, $location) {
     $scope.feedData = "loading..."
- //    xively.feed.get( $rootScope.feed_id, function( data ) {  
- //  		$scope.feedData=data;
- //  		$scope.$digest();
-	// }); 
-
-	var options = {
-		duration: "3hours",
-		interval: 10 // set the data points interval is 10s.
-	}
-	xively.feed.history( $rootScope.feed_id, options, function( data ) {  
+    xively.feed.get( $rootScope.feed_id, function( data ) {  
   		$scope.feedData=data;
   		$scope.$digest();
 	}); 
+
+	// var options = {
+	// 	duration: "3hours",
+	// 	interval: 10 // set the data points interval is 10s.
+	// }
+	// xively.feed.history( $rootScope.feed_id, options, function( data ) {  
+ //  		$scope.feedData=data;
+ //  		$scope.$digest();
+	// }); 
     
  }]);
